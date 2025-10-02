439cd529571b6ef4d75c0df8afbb2dd877216ff1
Nguyễn Viết Hoàn <viethoan22052006@gmail.com>
2025-10-02 09:19:44 +0700

    Initial commit

---
+++ TrainingC/FinalContest/problem-1/solution/sol.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define MOD 1000000007
+const int N = 1e6;
+#define MOD1 123456789
+
+
+void solve() {
+    
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
